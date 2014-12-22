526450a7d92f9ccfc9ad5c5348ed70344cac66b7
Matthias Ferber <matthias@cantinaconsulting.com>
2014-12-22 14:40:45 -0500

    Added Green Line route overlay

--- GreenT/GreenT/MapController.swift
+++ GreenT/GreenT/MapController.swift
@@ -23,6 +23,8 @@ class MapController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        placeGreenLineOverlay()
+        
         let statuses = MbtaApi.greenLineBTrainStatuses()
         if let statusesReal = statuses {
             for status in statusesReal {
@@ -40,12 +42,34 @@ class MapController: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewDidAppear(animated: Bool) {
+    override func viewWillAppear(animated: Bool) {
         let center = CLLocationCoordinate2D(latitude: 42.350570, longitude: -71.130660)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let startingRegion = MKCoordinateRegion(center: center, span: span)
         
-        mapView!.setRegion(startingRegion, animated: true)
+        mapView!.setRegion(startingRegion, animated: false)
+    }
+    
+    func placeGreenLineOverlay() {
+        let routeFileUrl: NSURL = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("greenline_b_aboveground_route", ofType: "csv")!)!
+        var error: NSError?
+        let csv = NSString(contentsOfURL: routeFileUrl, encoding: NSUTF8StringEncoding, error: &error)
+        if let csvReal = csv {
+            let lines = csvReal.componentsSeparatedByString("\n")
+            var points = [CLLocationCoordinate2D]()
+            for line in lines {
+                let coords = line.componentsSeparatedByString(",")
+                if (coords.count == 2) {
+                    let lat = (coords[0] as NSString).doubleValue
+                    let lon = (coords[1] as NSString).doubleValue
+                    var point = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                    points.append(point)
+                }
+            }
+            
+            var polyline = MKPolyline(coordinates: &points, count: points.count)
+            mapView.addOverlay(polyline)
+        }
     }
     
 
@@ -54,4 +78,15 @@ class MapController: UIViewController, MKMapViewDelegate {
 //    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
 //        <#code#>
 //    }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKPolyline {
+            var polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor(red: 0.06, green: 0.5, blue: 0.32, alpha: 1)
+            polylineRenderer.lineWidth = 8
+            return polylineRenderer
+        }
+        return nil
+
+    }
 }
